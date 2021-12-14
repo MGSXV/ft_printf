76452c375e49e6cc7a4fa6687c2992d84214ab5b
Soufiane Elkhamlichi <05soufiane@gmail.com>
2021-12-14 08:55:41 +0100

    Prints a charachter and increaments the int i

--- ft_putchar.c
+++ ft_putchar.c
@@ -6,8 +6,14 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 08:53:59 by sel-kham          #+#    #+#             */
-/*   Updated: 2021/12/14 08:54:13 by sel-kham         ###   ########.fr       */
+/*   Updated: 2021/12/14 08:54:48 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+void	ft_putchar(char c, int *i)
+{
+	write(1, &c, 1);
+	*i++;
+}
