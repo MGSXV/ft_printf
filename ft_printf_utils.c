905a0d19b62ce7538c4de9689045c4b26e876a9e
Soufiane Elkhamlichi <05soufiane@gmail.com>
2021-12-15 02:21:27 +0100

    Mendatory functions

--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -6,8 +6,30 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 19:25:42 by sel-kham          #+#    #+#             */
-/*   Updated: 2021/12/14 19:25:58 by sel-kham         ###   ########.fr       */
+/*   Updated: 2021/12/14 19:42:48 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+int  ft_strlen(char *s)
+{
+    int  i;
+
+    i = -1;
+    while (s[++i]);
+    return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int		i;
+
+	i = -1;
+	while (s[++i])
+		if (s[i] == (char) c)
+			return ((char *) s + i);
+	if (!c)
+		return ((char *) s + i);
+	return (0);
+}
